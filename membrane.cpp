e6429511fcd0a8b041fb950e65e36b46e72f6cbb
Spiros Kabasakalis <kabasakalis@gmail.com>
2017-08-23 11:14:28 +0300

    Added license

--- membrane.cpp
+++ membrane.cpp
@@ -1,3 +1,29 @@
+/*
+  Normal modes of a vibrating circular membrane (drumhead).
+  Membrane Class.
+  Qt 3D Surface implementation.
+  Copyright  2017 Spiros Kabasakalis <kabasakalis@gmail.com>
+
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the "Software"),
+  to deal in the Software without restriction, including without limitation
+  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+  and/or sell copies of the Software, and to permit persons to whom the
+  Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included
+  in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+  OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ **/
+
 #include "membrane.h"
 
 #include <QtCore/qmath.h>
@@ -89,11 +115,9 @@ void Membrane::updateTimeSlice() {
   m_membraneProxy->resetArray(m_resetArray);
 }
 
-
 void Membrane::setSelectedBesselOrder( int n) {
   m_selected_bessel_order = static_cast<float>(n);
 }
-
 
 void Membrane::setSelectedBesselRoot(int m) {
   m_selected_bessel_root = m;
@@ -145,7 +169,7 @@ void Membrane::setUpUi() {
   QGroupBox *normalModeGroupBox = new QGroupBox();
   QVBoxLayout *normalModeVBox = new QVBoxLayout;
 
-
+  // Normal modes form
   m_modeLabel = new QLabel(widget);
   m_modeLabel->setTextFormat(Qt::RichText);
   setModeLabel();
@@ -219,7 +243,6 @@ void Membrane::setUpUi() {
                      SLOT(setSelectedBesselOrder(int))) ;
   QObject::connect(besselRootSbx, SIGNAL(valueChanged(int)), this,
                      SLOT(setSelectedBesselRoot(int))) ;
-
   QObject::connect(modeNoneRB, &QRadioButton::toggled, this,
                    &Membrane::toggleModeNone);
   QObject::connect(modeItemRB, &QRadioButton::toggled, this,
